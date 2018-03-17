86f8184c9097c039e64d05999026a0506d8433e4
Akin Shonibare <akinkunmishonibare@gmail.com>
2018-03-16 22:32:13 -0400

    added index and login skeleton

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+    </title>
+  </head>
+  <body>
+      <h1>HELLO WORLD</h1>
+      <a href="login.php">login</a>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <title></title>
+  </head>
+  <body>
+    <h1>Login Page</h1>
+    <form action = "" method = "post">
+        <div class = "form-group">
+          <input type="text" name="username" class="form-control">
+        </div>
+        <div class = "form-group">
+          <input type="password" name="password" class="form-control">
+        </div>
+        <input class ="btn btn-success" type="submit" name="submit" value="submit">
+    </form>
+    </form>
+  </body>
+</html>
