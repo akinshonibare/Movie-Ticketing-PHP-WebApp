1d51ddf2108667a8b81ed97866dfee01c5b4b848
Akin Shonibare <akinkunmishonibare@gmail.com>
2018-03-19 16:41:45 -0400

    added navbar

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <title>
-    </title>
-  </head>
-  <body>
-      <h1>OMTS</h1>
-      <a href="login.php">login</a>
-  </body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <title>omts</title>
+    <style media="screen">
+      #cover {
+        width:100%;
+        height:100vh;
+      }
+    </style>
+</head>
+
+<body>
+    <section id="cover">
+        <nav class="navbar navbar-light navbar-expand-md" style="background-color:#808080;">
+            <div class="container-fluid"><a class="navbar-brand" href="#">OMTS</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="login.php">login</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="signUp.php">signUp</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+</body>
+
 </html>
